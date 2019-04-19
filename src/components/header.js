25f1b3ffc537d8fdf3a244a826fe28b9fe3a8109
camille villard <hello@camillevillard.com>
2019-04-19 10:25:05 -0400

    styled header component

--- src/components/header.js
+++ src/components/header.js
@@ -4,18 +4,15 @@ import React from "react"
 
 export default () => (
   <header>
-    <div>
-        <Link to="/">
-        {/* cam + alex */}
-        <div id="cam-alex">
-          <h6>cam + alex</h6>
-          <p>prout</p>
-          <div className="small-divider"></div>
-          <p>plus d'infos à venir,<br/>genre bientôt.</p>
-        </div>
-        {/* cam + alex */}
-        </Link>
-    </div>
+    <Link to="/">
+      {/* cam + alex */}
+      <div id="cam-alex">
+        <h6>cam + alex</h6>
+        <div className="small-divider"></div>
+      </div>
+      {/* cam + alex */}
+    </Link>
+    <p id="header-tagline">plus d'infos à venir,<br/>genre bientôt.</p>
   </header>
 )
 
