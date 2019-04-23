c81100f178399db0550eee73b63d16fcb531f46c
camille villard <hello@camillevillard.com>
2019-04-23 12:53:45 -0400

    added add to calendar link after successful rsvp

--- src/pages/success.jsx
+++ src/pages/success.jsx
@@ -30,6 +30,7 @@ class SuccessPage extends React.Component {
 
   render() {
     const data = defineContentLanguage(this.state.language, content);
+    const request = `https://calendar.google.com/calendar/r/eventedit?text=${data.eventName}&dates=20191019T170000Z/20191021T170000Z&location=${data.eventLocation}&sprop=name:Name&sprop=website:EventWebite&details=${data.eventDetails}&sf=true&output=xml`
     return(
       <div id="landing-page">
         <SEO id="success" title="bien reçu !" keywords={[`savethedate`, `dix neuf octobre`, `graphisme`]} />
@@ -41,7 +42,21 @@ class SuccessPage extends React.Component {
 
         <h1>{data.successHeader}</h1>
         <h5>{data.successTagline}</h5>
-        <Link to="/">{data.backToSite}</Link>
+
+        {/* links */}
+        <div id="links">
+          <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href={request}
+          >
+            {data.calendar}
+          </a>
+          <span>{' // '}</span>
+          <Link to="/">{data.backToSite}</Link>
+        </div>
+        {/* links */}
+
       </div>
     )
   }
