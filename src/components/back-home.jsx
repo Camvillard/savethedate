c36b525219e6abb23e70568aedbffbc28bcc4372
camille villard <hello@camillevillard.com>
2019-08-19 17:13:21 -0400

    typos & success page for payment

---
+++ src/components/back-home.jsx
@@ -0,0 +1,13 @@
+// external stuff
+import React from "react";
+import {Link} from "gatsby";
+
+const BackHome  = () => {
+  return(
+    <div className="back-home-btn">
+      <Link to="/">← accueil</Link>
+    </div>
+  )
+}
+
+export default BackHome;
